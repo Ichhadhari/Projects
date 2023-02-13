691fc7b1b1bc663d3feed5a0a8ec24f75779239f
Vaibhav Rokde <90153305+vaibhav-rokde@users.noreply.github.com>
2023-02-13 09:55:03 +0530

    Update settings.py

--- Computer Vision/Plant leaf classification API using Django/project_first/project_first/settings.py
+++ Computer Vision/Plant leaf classification API using Django/project_first/project_first/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+78mn)zrse-sg3i94)yv+c_t&ybyn=_1knbkjws6st7nis2_6r'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
